955fb54241a050294c68bab1ccf4e9b5a3b29f72
lucas.z.cai <lucas.z.cai@gmail.com>
2019-08-02 18:01:24 -0500

    added recycler view

--- app/src/main/java/com/scdevs/helpyourshelf/ShelvesFragment.java
+++ app/src/main/java/com/scdevs/helpyourshelf/ShelvesFragment.java
@@ -5,29 +5,52 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ShelvesFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ShelvesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ShelvesFragment extends Fragment {
+
+public class ShelvesFragment extends Fragment implements ShelvesRecyclerView.ItemClickListener{
     public static ShelvesFragment newInstance(){
         ShelvesFragment fragment = new ShelvesFragment();
         return fragment;
     }
 
+    ShelvesRecyclerView adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_shelves , container, false);
+
+
+        ArrayList<String> shelfnames = new ArrayList<>();
+        shelfnames.add("Home Bookshelf");
+        shelfnames.add("School Bookshelf");
+        shelfnames.add("Library Bookshelf");
+        shelfnames.add("Something Bookshelf");
+
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.shelvesrv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new ShelvesRecyclerView(getContext(), shelfnames);
+        recyclerView.setAdapter(adapter);
+        adapter.setClickListener(this);
+
         return view;
     }
+
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/scdevs/helpyourshelf/ShelvesRecyclerView.java
+++ app/src/main/java/com/scdevs/helpyourshelf/ShelvesRecyclerView.java
@@ -1,25 +1,67 @@
 package com.scdevs.helpyourshelf;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class ShelvesRecyclerView extends RecyclerView.Adapter{
+import java.util.List;
 
+public class ShelvesRecyclerView extends RecyclerView.Adapter<ShelvesRecyclerView.ViewHolder>{
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
 
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent) {
-        return null;
+    ShelvesRecyclerView(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ShelvesRecyclerView shelvesRecyclerView, int i) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.shelfitem, parent, false);
+        return new ViewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String name = mData.get(position);
+        holder.myTextView.setText(name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.shelfname);
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    String getItem(int id) {
+        return mData.get(id);
+    }
+
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
