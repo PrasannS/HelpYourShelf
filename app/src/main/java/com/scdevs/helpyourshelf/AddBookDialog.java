2aea0da38fc33cf838ebf96f505ca0c159b9fcab
lucas.z.cai <lucas.z.cai@gmail.com>
2019-08-02 23:23:20 -0500

    fa

---
+++ app/src/main/java/com/scdevs/helpyourshelf/AddBookDialog.java
@@ -0,0 +1,27 @@
+package com.scdevs.helpyourshelf;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialog;
+
+public class AddBookDialog extends AppCompatDialog {
+
+    public AddBookDialog(Context context) {
+        super(context);
+    }
+
+
+    public Dialog OnCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getOwnerActivity());
+        builder.setTitle("Add a Book Bitch").setMessage("that's right").setPositiveButton("ok", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){
+
+            }
+        });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/scdevs/helpyourshelf/ShelfActivity.java
+++ app/src/main/java/com/scdevs/helpyourshelf/ShelfActivity.java
@@ -1,7 +1,9 @@
 package com.scdevs.helpyourshelf;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.scdevs.helpyourshelf.DBModels.Book;
+import com.scdevs.helpyourshelf.DBModels.Shelf;
 
 import java.util.ArrayList;
 
@@ -27,7 +30,7 @@ public class ShelfActivity extends AppCompatActivity implements BooksRecyclerVie
             @Override
             public void onClick(View v) {
                 Toast.makeText(ShelfActivity.this, "add a book", Toast.LENGTH_SHORT).show();
-                //openDialog();
+                openDialog();
             }
         });
 
@@ -50,5 +53,24 @@ public class ShelfActivity extends AppCompatActivity implements BooksRecyclerVie
         startActivity(i);
     }
 
-    //public void
+    public void openDialog(){
+        //AddBookDialog addBookDialog = new AddBookDialog(this);
+        //addBookDialog.show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Add a Book");
+        builder.setMessage("Title");
+
+        builder.
+
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                ShelfActivity.this.finish();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 }
