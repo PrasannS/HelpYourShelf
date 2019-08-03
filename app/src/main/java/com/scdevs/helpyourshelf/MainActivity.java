99e3d5f8c11c466f70d21023157b145a9b16266f
TaeminHa <tommy7507@gmail.com>
2019-08-02 19:09:35 -0500

    Book info page UI (only xml) done

--- app/src/main/java/com/scdevs/helpyourshelf/MainActivity.java
+++ app/src/main/java/com/scdevs/helpyourshelf/MainActivity.java
@@ -1,13 +1,19 @@
 package com.scdevs.helpyourshelf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+	private Button to_book_info;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
 	}
 }
