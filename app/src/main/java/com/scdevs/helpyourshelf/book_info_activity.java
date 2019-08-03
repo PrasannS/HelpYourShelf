cce3bf537adfb5ed1eef4ff6566f8ff8c424894e
TaeminHa <tommy7507@gmail.com>
2019-08-02 20:13:53 -0500

    Merge branch 'Samuel' of https://github.com/PrasannS/HelpYourShelf
    # Conflicts:
    #	app/build.gradle
    #	openCVLibrary347/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
    #	openCVLibrary347/build/outputs/logs/manifest-merger-debug-report.txt

--- app/src/main/java/com/scdevs/helpyourshelf/book_info_activity.java
+++ app/src/main/java/com/scdevs/helpyourshelf/book_info_activity.java
@@ -3,14 +3,23 @@ package com.scdevs.helpyourshelf;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class book_info_activity extends AppCompatActivity {
 
+    private ImageView imageView;
     private Button toShelf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_info_activity);
+
+        imageView = findViewById(R.id.book_image);
+
+        String url = "http://books.google.com/books/content?id=RFVoAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api";
+        Picasso.with(this).load(url).into(imageView);
     }
 }
