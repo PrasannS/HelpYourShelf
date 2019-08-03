a150ecd945a32e2c1755241930f7bb9bea249471
TaeminHa <tommy7507@gmail.com>
2019-08-02 21:20:03 -0500

    Merge branch 'Samuel' of https://github.com/PrasannS/HelpYourShelf
    # Conflicts:
    #	app/build.gradle
    #	openCVLibrary347/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
    #	openCVLibrary347/build/outputs/logs/manifest-merger-debug-report.txt

--- app/src/main/java/com/scdevs/helpyourshelf/book_info_activity.java
+++ app/src/main/java/com/scdevs/helpyourshelf/book_info_activity.java
@@ -1,25 +1,34 @@
 package com.scdevs.helpyourshelf;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import me.relex.photodraweeview.PhotoDraweeView;
 
 public class book_info_activity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button toShelf;
 
+    private String url = "http://books.google.com/books/content?id=RFVoAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_info_activity);
 
-        imageView = findViewById(R.id.book_image);
+        PhotoDraweeView drawerView = (PhotoDraweeView) findViewById(R.id.book_image);
+        drawerView.setPhotoUri(Uri.parse(url));
 
+        /*
+        picaso not working
         String url = "http://books.google.com/books/content?id=RFVoAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api";
-        Picasso.with(this).load(url).into(imageView);
+        Picasso.with(this).load(url).into(imageView); */
     }
 }
